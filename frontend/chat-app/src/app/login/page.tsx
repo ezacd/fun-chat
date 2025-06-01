639f7ea6dc96f210efd1c311c7994db9cf845386
ezacd <yaroslav.us2312@gmail.com>
2025-06-01 13:57:09 +0300

    feat: react hook form

--- frontend/chat-app/src/app/login/page.tsx
+++ frontend/chat-app/src/app/login/page.tsx
@@ -1,20 +1,46 @@
+'use client';
+
 import Link from 'next/link';
 import styles from './login.module.css';
+import { useForm } from 'react-hook-form';
 
-export default function Lofin() {
+type Inputs = {
+  email: string;
+  password: string;
+};
+
+export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const submitForm = (data: Inputs) => {
+    console.log(data);
+  };
+
   return (
     <div className={styles.login}>
       <div className={styles.loginBox}>
         <h2 className={styles.loginText}>Login</h2>
-        <form className={styles.loginForm}>
+        <form className={styles.loginForm} onSubmit={handleSubmit(submitForm)}>
           <label className={styles.loginFormLabel}>
             E-mail:
-            <input className={styles.loginFromInput} type="email" />
+            <input
+              className={styles.loginFromInput}
+              type="email"
+              {...register('email')}
+            />
           </label>
 
           <label className={styles.loginFormLabel}>
             Password:
-            <input className={styles.loginFromInput} type="password" />
+            <input
+              className={styles.loginFromInput}
+              type="password"
+              {...register('password')}
+            />
           </label>
 
           <button className={styles.loginFormSubmit} type="submit">
--- frontend/chat-app/src/app/register/page.tsx
+++ frontend/chat-app/src/app/register/page.tsx
@@ -1,25 +1,59 @@
+'use client';
+
 import Link from 'next/link';
 import styles from './register.module.css';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+type Inputs = {
+  email: string;
+  password: string;
+  confPassword: string;
+};
 
 export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const submitForm = (data: Inputs) => {
+    console.log(data);
+  };
+
   return (
     <div className={styles.register}>
       <div className={styles.registerBox}>
         <h2 className={styles.registerText}>Register</h2>
-        <form className={styles.registerForm}>
+        <form
+          className={styles.registerForm}
+          onSubmit={handleSubmit(submitForm)}
+        >
           <label className={styles.registerFormLabel}>
             E-mail:
-            <input className={styles.registerFromInput} type="email" />
+            <input
+              className={styles.registerFromInput}
+              type="email"
+              {...register('email')}
+            />
           </label>
 
           <label className={styles.registerFormLabel}>
             Password:
-            <input className={styles.registerFromInput} type="password" />
+            <input
+              className={styles.registerFromInput}
+              type="password"
+              {...register('password')}
+            />
           </label>
 
           <label className={styles.registerFormLabel}>
             Confium password:
-            <input className={styles.registerFromInput} type="password" />
+            <input
+              className={styles.registerFromInput}
+              type="password"
+              {...register('confPassword')}
+            />
           </label>
 
           <button className={styles.registerFormSubmit} type="submit">
