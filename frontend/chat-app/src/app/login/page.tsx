1fe58815689431da9196650d8fcadc365e352f3c
ezacd <yaroslav.us2312@gmail.com>
2025-06-01 12:38:39 +0300

    feat: login page styles

--- frontend/chat-app/src/app/login/page.tsx
+++ frontend/chat-app/src/app/login/page.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <>login</>;
+import styles from './login.module.css';
+
+export default function Lofin() {
+  return (
+    <div className={styles.login}>
+      <div className={styles.loginBox}>
+        <h2 className={styles.loginText}>Login</h2>
+        <form className={styles.loginForm}>
+          <label className={styles.loginFormLabel}>
+            E-mail:
+            <input className={styles.loginFromInput} type="email" />
+          </label>
+
+          <label className={styles.loginFormLabel}>
+            Password:
+            <input className={styles.loginFromInput} type="password" />
+          </label>
+
+          <button className={styles.loginFormSubmit} type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
--- frontend/chat-app/src/app/register/page.tsx
+++ frontend/chat-app/src/app/register/page.tsx
@@ -1,3 +1,31 @@
-export default function Home() {
-  return <>register</>;
+import styles from './register.module.css';
+
+export default function Register() {
+  return (
+    <div className={styles.register}>
+      <div className={styles.registerBox}>
+        <h2 className={styles.registerText}>Register</h2>
+        <form className={styles.registerForm}>
+          <label className={styles.registerFormLabel}>
+            E-mail:
+            <input className={styles.registerFromInput} type="email" />
+          </label>
+
+          <label className={styles.registerFormLabel}>
+            Password:
+            <input className={styles.registerFromInput} type="password" />
+          </label>
+
+          <label className={styles.registerFormLabel}>
+            Confium password:
+            <input className={styles.registerFromInput} type="password" />
+          </label>
+
+          <button className={styles.registerFormSubmit} type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
