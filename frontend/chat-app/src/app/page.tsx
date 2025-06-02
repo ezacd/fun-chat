1e1b41770128b88d9e72c748df84397e752fe245
ezacd <yaroslav.us2312@gmail.com>
2025-06-02 17:37:31 +0300

    feat: redirect user after log out

--- frontend/chat-app/src/app/page.tsx
+++ frontend/chat-app/src/app/page.tsx
@@ -1,10 +1,14 @@
 'use client';
 
 import { deleteCookie } from 'cookies-next';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
+
   const logOut = () => {
     deleteCookie('token');
+    router.push('/login');
   };
 
   return <button onClick={logOut}>Log Out</button>;
--- frontend/chat-app/src/middleware.ts
+++ frontend/chat-app/src/middleware.ts
@@ -2,7 +2,6 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest) {
   const token = req.cookies.get('token')?.value;
-  console.log('Token:', token);
 
   if (!token) {
     return NextResponse.redirect(new URL('/login', req.url));
