36a40ff8670b487320d2573eabc4b56e88617f94
ezacd <yaroslav.us2312@gmail.com>
2025-06-09 13:29:01 +0300

    fix: middleware

--- frontend/chat-app/src/middleware.ts
+++ frontend/chat-app/src/middleware.ts
@@ -2,14 +2,19 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest) {
   const token = req.cookies.get('token')?.value;
+  const { pathname } = req.nextUrl;
 
   if (!token) {
     return NextResponse.redirect(new URL('/login', req.url));
+  }
+
+  if (token && (pathname === '/login' || pathname === '/register')) {
+    return NextResponse.redirect(new URL('/', req.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/'],
+  matcher: ['/', '/login', '/register'],
 };
